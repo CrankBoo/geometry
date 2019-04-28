416bbd6f21e8c318cf139f9764d3cd1320c3cc11
BaronSameda <Sameda@localhost.localdomain>
2019-04-28 12:30:31 -0400

    create ignore

--- main.c
+++ main.c
@@ -7,11 +7,11 @@ int main()
 	float x1,y1,r1,x2,y2,r2,r;
 	float dist;
 	printf ("circle #1: ");
-	scanf ("%f%f%f ",&x1&y1,&r1);
+	scanf ("%f%f%f ",&x1,&y1,&r1);
 	
 	printf ("\n\ncircle #2: ");
 	scanf ("%f%f%f", &x2,&y2,&r2);
-	dist = sqrt ((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+	dist = sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 	r=r1+r2;
 	printf ("\n\n 1.circle (%.2f %.2f %.2f)",x1,y1,r1);
 	printf ("\n Perimitr = %.2f",2*3.14*r1);
